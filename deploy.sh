ac1cc104cc2aaa55c62171a46fd7bf8d252166fc
もじょんぬ <moe.e.coop@gmail.com>
2020-05-06 14:12:57 +0900

    デプロイスクリプトの細かい変更

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,5 @@ git config user.name 'Travis CI'
 git config user.email "$MOECOOP_MAIL"
 git add -A
 git commit -m "Deploy to Github Pages"
-git push -f -q "https://${GH_TOKEN}@github.com/${MOECOOP_REPO}.git" master:gh-pages > /dev/null 2>&1
+echo "Push moecoop-webui to Github Pages"
+git push -f -q "https://${GH_TOKEN}@github.com/${MOECOOP_REPO}.git" master:gh-pages
